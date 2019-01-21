28ed42298ae1385cf7f6b8848624ce470b3011f9
“Peter <2296505z@student.gla.ac.uk>
2019-01-21 09:24:23 +0000

    chapter 3 excercises done, created about page

--- rango/urls.py
+++ rango/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from rango import views
+from django.conf.urls import include
+
 
 urlpatterns = [
-	url(r'^$', views.index, name='index')
+	url(r'^$', views.index, name='index'),
+	url(r'about/', views.about, name ='about')
 ]
--- rango/views.py
+++ rango/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hey there partner!")
+	return HttpResponse("Rango says hey there partner!\n<br/><a href =\"/rango/about\">About this page</a>")
 # Create your views here.
+
+def about(request): 
+	return HttpResponse("<a href=\"/rango/\"> Index</a>")
