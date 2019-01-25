ab0b1da15192b6b42cbccf8745d10ac86f696a10
“Peter <2296505z@student.gla.ac.uk>
2019-01-25 04:49:46 +0000

    Beginning of chapter 6

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.models import Category
 
 def index(request):
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-	return render(request, 'rango/index.html', context=context_dict)
+	# Query the database for a list of ALL categories currently stored.
+	# Order the categories by no. likes in descending order.
+	# Retrieve the top 5 only - or all if less than 5.
+	# Place the list in our context_dict dictionary
+	# that will be passed to the template engine.
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list}
+	# Render the response and send it back!
+	return render(request, 'rango/index.html', context_dict)
 
 	# return HttpResponse("Rango says hey there partner!\n<br/><a href =\"/rango/about\">About this page</a>")
 # Create your views here.
