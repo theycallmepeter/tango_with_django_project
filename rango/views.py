cbd76dc901d425bacc83d3050ad4c9858ca439d9
“Peter <2296505z@student.gla.ac.uk>
2019-02-07 00:12:00 +0000

    Chapter 10 complete, server side cookies + views

--- rango/views.py
+++ rango/views.py
@@ -7,41 +7,50 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
-def visitor_cookie_handler(request, response):
+
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+
+
+def visitor_cookie_handler(request):
 	# Get the number of visits to the site.
 	# We use the COOKIES.get() function to obtain the visits cookie.
 	# If the cookie exists, the value returned is casted to an integer.
 	# If the cookie doesn't exist, then the default value of 1 is used.
-	visits = int(request.COOKIES.get('visits', '1'))
-	last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
-	last_visit_time = datetime.strptime(
-		last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request,
+											   'last_visit',
+											   str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+											'%Y-%m-%d %H:%M:%S')
 	# If it's been more than a day since the last visit...
 	if (datetime.now() - last_visit_time).days > 0:
 		visits = visits + 1
 		# Update the last visit cookie now that we have updated the count
-		response.set_cookie('last_visit', str(datetime.now()))
+		request.session['last_visit'] = str(datetime.now())
 	else:
 		# Set the last visit cookie
-		response.set_cookie('last_visit', last_visit_cookie)
+		request.session['last_visit'] = last_visit_cookie
 
 	# Update/set the visits cookie
-	response.set_cookie('visits', visits)
+	request.session['visits'] = visits
 
 
 def index(request):
 	request.session.set_test_cookie()
 	category_list = Category.objects.order_by('-likes')[:5]
 	page_list = Page.objects.order_by('-views')[:5]
-	context_dict = {'categories': category_list, 'pages': page_list, 'visits':request.COOKIES.get('visits')}
+	context_dict = {'categories': category_list,
+					'pages': page_list}
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
 
-	response = render(request, 'rango/index.html', context_dict)
-
-	visitor_cookie_handler(request, response)
-	return response
 	# Render the response and send it back!
-	return render(request, 'rango/index.html', context_dict)
-
+	response =  render(request, 'rango/index.html', context_dict)
+	return response
 	# return HttpResponse("Rango says hey there partner!\n<br/><a href =\"/rango/about\">About this page</a>")
 # Create your views here.
 
